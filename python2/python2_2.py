b06c4426b0351436a0e3f627c31074fea9fd76e5
Kasidit Samad <111700359+Ditokasiditz@users.noreply.github.com>
2023-07-11 11:10:02 +0700

    Update python2_2.py

--- python2/python2_2.py
+++ python2/python2_2.py
@@ -1,21 +1,20 @@
-from math import pi
-
+pi = 3.141592653589793
 class Spherical:
 
     def __init__(self,r1):
-        self.r = r1
+        self.radius = r1
 
     def changeR(self,r2):
-        self.r = r2
+        self.radius = r2
 
     def findVolume(self):
-        return (4/3)*pi*pow(self.r,3)
+        return (4/3)*pi*pow(self.radius,3)
 
     def findArea(self):
-        return 4*pi*pow(self.r,2)
+        return 4*pi*pow(self.radius,2)
 
     def __str__(self):
-        return f'Radius ={self.r} Volumn = {Spherical.findVolume} Area = {Spherical.findArea}' 
+        return f'Radius ={self.radius} Volumn = {self.findVolume()} Area = {self.findArea()}' 
 
 r1, r2 = input("Enter R : ").split()
 R1 = Spherical(int(r1))
