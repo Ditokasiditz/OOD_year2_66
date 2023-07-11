c4d13f545218f50a833c83e840ccfa028da939ac
Kasidit Samad <111700359+Ditokasiditz@users.noreply.github.com>
2023-07-11 10:05:48 +0700

    add python2_1

--- python2/python2_1.py
+++ python2/python2_1.py
@@ -1,6 +1,27 @@
-def Rshift(num,shift):
-  return num >> shift
-  
-n,s = input("Enter number and shiftcount : ").split()
+class Calculator :
+  def __init__(self,x,y) :
+    self.num1 = x
+    self.num2 = y
+  def add(self):
+    return self.num1+self.num2
 
-print(Rshift(int(n),int(s)))
+  def sub(self):
+    return self.num1-self.num2
+
+  def mul(self):
+    return self.num1*self.num2
+
+  def truediv(self):
+    return self.num1/self.num2
+
+  
+
+x,y = input("Enter num1 num2 : ").split(",")
+
+Cal1 = Calculator(int(x),int(y))
+
+
+print(Cal1.add())
+print(Cal1.sub())
+print(Cal1.mul())
+print(Cal1.truediv())
