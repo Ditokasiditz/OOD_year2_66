c7cefec82556c0224af18b93d95d1fc03fb691f2
Kasidit Samad <111700359+Ditokasiditz@users.noreply.github.com>
2023-08-30 23:19:24 +0700

    Update 2.py

--- Recursion/2.py
+++ Recursion/2.py
@@ -20,19 +20,18 @@ def remove_demon(village_map, x, y):
         village_map = remove_demon(village_map, x, y-1)
     return village_map
 
-
-
 # Get input
 width, height = map(int, input("Enter input: ").split())
 village_map_inp = [input() for _ in range(height)]
 
-#fix village map
+print(village_map_inp)
+# Fix village map
 village_map = []
 for row in village_map_inp:
-        row_without_spaces = row.replace(' ', '')  # Remove spaces from the row
-        village_map.append(row_without_spaces)  # Add the modified row to the fix_village_map
-    
+    row_without_spaces = row.replace(' ', '')  # Remove spaces from the row
+    village_map.append(row_without_spaces)  # Add the modified row to the fix_village_map
 
+print(village_map)
 # Calculate and print the result
 result = count_demons(village_map)
 print(result)
